6bdae58f44c8546d57fa883df1f3f28058656485
dariocast <dariocastellano18@gmail.com>
2016-10-13 16:30:54 +0200

    now plugin should show all user's badges

--- controllers/badge.php
+++ controllers/badge.php
@@ -11,6 +11,7 @@ class GAMIFICATION_CTRL_Badge extends OW_ActionController {
         $this->setPageHeading("Tutti i tuoi badge");
 
         $userId = OW::getUser();
+        GAMIFICATION_BOL_Service::getInstance()->addBadge("nuovo",$userId);
         $myBadges = GAMIFICATION_BOL_Service::getInstance()->findListByUserId($userId);
 
         $this->assign("myBadges",$myBadges);
